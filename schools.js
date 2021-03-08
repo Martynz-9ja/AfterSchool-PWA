a450cc1641c327d20ffe0e8d7900e6cebe2c55f0
Martins <mnonuminya@yahoo.com>
2021-03-08 19:41:12 +0100

    first commit

---
+++ schools.js
@@ -0,0 +1,82 @@
+let schools = [
+    {
+        "id": 1001,
+        "subject":'Math',
+        "location":'Ikoyi',
+        "price":100,
+        "image":"images/math.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 1002,
+        "subject":'Arts & Crafts',
+        "location":'Yaba',
+        "price":80,
+        "image":"images/arts.jpeg",
+        "availableInventory": 5
+    },
+    {
+        "id": 1003,
+       "subject":'Cooking',
+        "location":'Lekki',
+        "price":150,
+        "image":"images/cooking.jpeg",
+        "availableInventory": 5
+    },
+    {
+        "id": 1004,
+        "subject":'Dance',
+        "location":'Mushin',
+        "price":25,
+        "image":"images/dance.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 1005,
+        "subject":'Football',
+        "location":'Surulere',
+        "price":100,
+        "image":"images/football.jpeg",
+        "availableInventory": 5
+    },
+    {
+        "id": 1006,
+        "subject":'Music',
+        "location":'Agege',
+        "price":150,
+        "image":"images/music.jpeg",
+        "availableInventory": 5
+    },
+    {
+        "id": 1007,
+        "subject":'Photography',
+        "location":'London',
+        "price":60,
+        "image":"images/photography.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 1008,
+        "subject":'Robotics',
+        "location":'Epe',
+        "price":300,
+        "image":"images/robotic.png",
+        "availableInventory": 5
+    },
+    {
+        "id": 1009,
+        "subject":'Swimming',
+        "location":'Apapa',
+        "price":100,
+        "image":"images/swimming.jpeg",
+        "availableInventory": 5
+    },
+    {
+        "id": 1010,
+        "subject":'Track and Field',
+        "location":'Ikeja',
+        "price":150,
+        "image":"images/track.jpeg",
+        "availableInventory": 5
+    }
+]
